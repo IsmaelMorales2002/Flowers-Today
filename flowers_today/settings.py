1c5427eaf0bda4dcd572d30eeb65c6727f18193d
IsmaelMorales2002 <104656736+IsmaelMorales2002@users.noreply.github.com>
2025-06-11 20:14:10 -0600

    Configuracion Conexion BD

--- flowers_today/settings.py
+++ flowers_today/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'flowers_today.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'flowers',
+        'USER': 'postgres',
+        'PASSWORD': 'root',
+        'HOST': 'localhost',
+        'PORT': '5432'
     }
 }
 
