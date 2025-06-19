8ec616e73d899de58f167e9c6b0885bdd48d5f8a
IsmaelMorales2002 <104656736+IsmaelMorales2002@users.noreply.github.com>
2025-06-19 15:30:09 -0600

    Comentarios Agregados

--- app/views.py
+++ app/views.py
@@ -2,12 +2,10 @@ from django.shortcuts import render,redirect
 
 # Create your views here.
 
-"""
-Funcion Login, donde se validara la contraseña y correo electronico
-para poder iniciar sesion.
-"""
+#Funcion Login, Muestra la vista Login.html
 def Login(request):
     return render(request,'login.html')
 
+#Funcion Crear_Cuenta, Muestra la vista registro.html
 def Crear_Cuenta(request):
     return render(request,'registro.html')
