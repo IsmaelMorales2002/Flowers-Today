56294baa7fd41ebeddbde0964cbd491fbe3c32ca
IsmaelMorales2002 <104656736+IsmaelMorales2002@users.noreply.github.com>
2025-06-19 19:51:22 -0600

    Toggle Js Agregado

---
+++ static/js/toggle.js
@@ -0,0 +1,10 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const togglePassword = document.getElementById('togglePassword');
+    const passwordInput = document.getElementById('password');
+
+    togglePassword.addEventListener('click', function () {
+        const isPassword = passwordInput.type === 'password';
+        passwordInput.type = isPassword ? 'text' : 'password';
+        togglePassword.textContent = isPassword ? 'Ocultar' : 'Mostrar';
+    });
+});
