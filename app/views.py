7b3e4e00a291af8919bb547cb445084fbf558385
IsmaelMorales2002 <104656736+IsmaelMorales2002@users.noreply.github.com>
2025-06-19 19:43:03 -0600

    Views Modificada Crear_Cuenta_Cliente

--- app/views.py
+++ app/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render,redirect
 from django.contrib import messages
 from .models import *
 from django.db.models import Q
+from django.http import JsonResponse
 
 # Create your views here.
 
@@ -27,51 +28,4 @@ def Crear_Cuenta_Cliente(request):
     password = request.POST.get('txtPasswordN','').strip()
 
     if not all([nombre,apellido,telefono,correo,password]):
-        messages.error(request,'Dejo Un Campo Vacio, Cuenta No Creada')
-
-    #Validacion para crear registro en Tabla Usuario
-    try:
-        #Validacion para crear registro en Tabla ROL
-        # try: 
-        #     #Creacion de Registro Rol
-        #     rol = Rol(
-        #         nombre_rol = 'C'
-        #     )
-        #     rol.save()
-        # except Exception:
-        #     messages.error(request,'Error Al Crear La Cuenta')
-        #     return redirect('registro')
-            #Terminar Validacion ROL
-
-        #Validacion De Correo Y Telefono
-        try:
-            datosExistentes = Usuario.objects.filter(
-                Q(correo_usuario = correo) | Q(telefono_usuario = telefono)).first()
-            if datosExistentes:
-                messages.warning(request,'Correo o Telefono Ya Registrado')
-                contexto = {
-                    'nombre': nombre,
-                    'apellido': apellido,
-                    'telefono': telefono,
-                    'correo': correo,
-                }
-                return render(request,'registro.html',contexto)
-            
-            #Creacion de Registro Usuario
-            usuario = Usuario(
-                # id_rol = rol,
-                nombre_usuario = nombre,
-                apellido_usuario = apellido,
-                telefono_usuario = telefono,
-                correo_usuario = correo,
-                password_usuario = password,
-                usuario_activo = True
-            )
-            print('PASO LA PRUEBA')
-            # usuario.save()
-            return redirect('login')
-        except Exception as e:
-            return redirect('registro')
-    except Exception as e:
-        messages.error(request,'Actualmente No Se Puede Registrar Su Cuenta')
-        return  render(request,'login.html')
+        return JsonResponse({'Ok': False, 'mensaje':'Todos los campos son obligatorios'})
--- static/js/toggle.js
+++
@@ -1,8 +0,0 @@
-document.getElementById('togglePassword').addEventListener('click', function() {
-    const passwordInput = document.getElementById('password');
-    const confirmPasswordInput = document.getElementById('confirmPassword');
-    const type = passwordInput.type === 'password' ? 'text' : 'password';
-    passwordInput.type = type;
-    confirmPasswordInput.type = type;
-    this.textContent = type === 'password' ? 'Mostrar' : 'Ocultar';
-});
