1d5b653f49d638941d69cb4ad0b3e527463c0d79
Edwin Quintanilla <qg18004@ues.edu.sv>
2025-06-23 21:10:10 -0600

    sync main

--- flowers_today/settings.py
+++ flowers_today/settings.py
@@ -83,7 +83,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'flowers',
         'USER': 'postgres',
-        'PASSWORD': 'root',
+        'PASSWORD': '1234',
         'HOST': 'localhost',
         'PORT': '5432'
     }
