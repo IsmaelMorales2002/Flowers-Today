5d98f1d8850262844d6178ae0ecec9eb8c82abbc
IsmaelMorales2002 <104656736+IsmaelMorales2002@users.noreply.github.com>
2025-10-11 23:54:52 -0600

    Carrito Actualizado

--- static/js/carritoState.js
+++ static/js/carritoState.js
@@ -14,19 +14,21 @@ document.addEventListener('DOMContentLoaded', () =>{
 
     document.querySelectorAll('.btn-agregar').forEach(btn =>{
         btn.addEventListener('click', () =>{
+            let carrito_data = JSON.parse(localStorage.getItem('carrito')) || []
+            
+            
             if(btn.classList.contains("agregado")){
-
+                
+                
             }else{
-                let carrito_data = JSON.parse(localStorage.getItem('carrito')) || []
-
                 let id = btn.dataset.id
                 let nombre = btn.dataset.nombre
                 let imagen = btn.dataset.imagen
                 let precio = btn.dataset.precio
                 let descripcion = btn.dataset.descripcion
-
+    
                 let producto = {id,nombre,imagen,precio,descripcion}
-
+    
                 carrito_data.push(producto)
                 localStorage.setItem('carrito',JSON.stringify(carrito_data))
                 actualizarIconoCarrito();
