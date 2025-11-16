19672a2fe257419951877324719f33265d6fde95
IsmaelMorales2002 <104656736+IsmaelMorales2002@users.noreply.github.com>
2025-11-16 10:45:54 -0600

    Servidor Produccion

--- flowers_today/settings.py
+++ flowers_today/settings.py
@@ -13,6 +13,10 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 import os
 from pathlib import Path
 
+print("USER:", os.environ.get("BREVO_SMTP_USER"))
+print("KEY:", os.environ.get("BREVO_SMTP_KEY"))
+print("FROM:", os.environ.get("BREVO_SMTP_FROM"))
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
